6109cfe8d0d2cb41c25be0a6978ab8740a73f580
Michael Peng <michael.tk.peng@gmail.com>
2021-12-27 11:45:23 -0700

    Project forecaster using graphql and lambda, built using amplify.

---
+++ amplify/backend/function/calculateProjectForecast/ts/API.ts
@@ -0,0 +1,13 @@
+/* tslint:disable */
+/* eslint-disable */
+//  This file was automatically generated and should not be edited.
+
+export type GetProjectForecastQueryVariables = {
+  velocities: Array< number >,
+  target: number,
+  confidence?: number | null,
+};
+
+export type GetProjectForecastQuery = {
+  getProjectForecast?: number | null,
+};
---
+++ amplify/backend/function/calculateProjectForecast/ts/index.ts
@@ -0,0 +1,44 @@
+import {
+    AppSyncResolverEvent
+} from 'aws-lambda';
+import type {GetProjectForecastQueryVariables} from "./API"
+
+const simulationRuns = 10000
+
+export const handler = async (
+    event: AppSyncResolverEvent<GetProjectForecastQueryVariables, number>
+): Promise<number> => {
+    let confidence = 0.5;
+    const conf = event.arguments.confidence;
+
+    if (conf == null || conf <= 0) {
+    } else if (conf > 1) {
+        confidence = 1;
+    } else if (conf > 0) {
+        confidence = conf;
+    }
+
+    return projectForecast(event.arguments.velocities, event.arguments.target, confidence);
+};
+
+async function projectForecast(velocities: number[], target: number, confidence: number, runs = simulationRuns): Promise<number> {
+    const forecasts = await Promise.all([...Array(runs)].map(async (_, i) => singleForecast(velocities, target)));
+
+    return forecasts.sort(function(a, b) {return a - b})[Math.floor((runs - 1) * confidence)];
+}
+
+function singleForecast(velocities: number[], target: number): number {
+    let remain = target;
+    let forecast = 0;
+
+    while (remain > 0) {
+        forecast += 1;
+        remain -= iterationForecast(velocities);
+    }
+
+    return forecast;
+}
+
+function iterationForecast(velocities: number[]): number {
+    return velocities[Math.floor(Math.random() * velocities.length)];
+}
